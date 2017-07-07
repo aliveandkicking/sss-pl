e42cece0845d3819d070a02976d17bb333805e50
Viktor Livar <Viktor_Livar@epam.com>
2017-07-07 23:39:07 +0300

    the first project commit

---
+++ client-react/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+
+  <App />
+  ,
+  document.getElementById('root')
+);
+registerServiceWorker();
---
+++ client-shared/reducers/index.js
@@ -0,0 +1,22 @@
+const page = (state = 'week', action) => {
+  return state;
+};
+
+const isEditingTask = (state = false, action) {
+  switch (action.type) {
+    case value:
+
+      break;
+
+    default:
+      break;
+  }
+
+
+
+  if (action.type === 'EDIT_TASK') {
+
+  }
+  }
+  return action.isEditingTask
+}
