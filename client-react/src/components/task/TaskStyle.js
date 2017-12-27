a39b9910f910e4bcd9d2f1afb74c07c870680416
orangetoad <thebiktop@gmail.com>
2017-12-27 23:34:27 +0200

    def. task color

--- client-react/src/components/task/TaskStyle.js
+++ client-react/src/components/task/TaskStyle.js
@@ -29,7 +29,7 @@ export const taskStyles = {
     height: '100%',
     width: '100%',
     transition: '200ms',
-    backgroundColor: '#666'
+    backgroundColor: '#99d'
   },
   contentHover: {
     boxShadow: 'inset 0px 0px 1px rgba(255,255,255,1)'
